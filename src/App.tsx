8b5d2279ca70833ae56a012f31a12810d4086b1a
JoaoPedro191 <sasc.joao@gmail.com>
2024-07-15 14:40:07 -0300

    primeio commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6">  
+      saojasoasjo
+      </div>
+    </div>
+  )
+}
+//28:52
+export { App } 
